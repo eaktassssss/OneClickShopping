b624d91811421577e1834900c1b35a60fee9702e
eaktas <eaktas@innova.com.tr>
2022-09-29 16:53:41 +0300

    Add project files.

---
+++ OneClickShopping.Managment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OneClickShopping.Managment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
